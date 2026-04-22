20358f6ee8fb10eff514928c7d17be0f39abcc60
Daniel A <instalaciones@bitss.com.mx>
2026-04-22 10:17:59 -0600

    estructura inicial del proyecto, tema y supasabase config

---
+++ src/components/providers/supabase-provider.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { createContext, useContext, useState } from 'react'
+import { createClient } from '@/lib/supabase'
+import type { SupabaseClient } from '@supabase/supabase-js'
+import type { Database } from '@/types/database.types'
+
+// Definimos qué información va a "flotar" por la app
+type SupabaseContext = {
+  supabase: SupabaseClient<Database>
+}
+
+const Context = createContext<SupabaseContext | undefined>(undefined)
+
+export default function SupabaseProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  // Inicializamos el cliente una sola vez
+  const [supabase] = useState(() => createClient())
+
+  return <Context.Provider value={{ supabase }}>{children}</Context.Provider>
+}
+
+// Este es el "gancho" (hook) que usaremos en cualquier parte para llamar a Supabase
+export const useSupabase = () => {
+  const context = useContext(Context)
+  if (context === undefined) {
+    throw new Error('useSupabase debe usarse dentro de un SupabaseProvider')
+  }
+  return context
+}
